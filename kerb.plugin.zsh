bb4f9042cf2b4e3428c86142c7c89bf857ab7c20
Alex Kilgour <alexjkilgour@gmail.com>
2019-04-01 14:25:08 +0100

    update plugin test

--- kerb.plugin.zsh
+++ kerb.plugin.zsh
@@ -2,6 +2,6 @@ local PLUGIN_DIR="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
 local PLUGIN_STR="https-COLON--SLASH--SLASH-github.com-SLASH-alexkilgour-SLASH-kerbside"
 
 # $( cd "$PLUGIN_DIR" && npm install )
-[ ! -d "$PLUGIN_DIR/$PLUGIN_STR/node_modules" ] && cd $PLUGIN_DIR/$PLUGIN_STR && npm install
+[ ! -d "$PLUGIN_DIR/$PLUGIN_STR/node_modules" ] && npm --prefix $PLUGIN_DIR/$PLUGIN_STR install
 
 alias kerb="$PLUGIN_DIR/bin/kerbside"
