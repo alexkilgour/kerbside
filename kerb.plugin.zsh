785eddc65ea8159cc0a8cf428e02927edcdfc179
Alex Kilgour <alexjkilgour@gmail.com>
2019-04-01 15:19:24 +0100

    better today functionality

--- kerb.plugin.zsh
+++ kerb.plugin.zsh
@@ -4,3 +4,4 @@ local PLUGIN_DIR="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
 [ ! -d "$PLUGIN_DIR/node_modules" ] && npm --prefix $PLUGIN_DIR install
 
 alias kerb="$PLUGIN_DIR/bin/kerbside"
+alias kerbtoday="$PLUGIN_DIR/bin/kerbside -t"
